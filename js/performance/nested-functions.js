259aeb65bb021440558d1e1acdcc7c025003475c
Thorsten Lorenz <thlorenz@gmx.de>
2012-06-02 19:28:30 -0400

    more realistic test

--- js/performance/nested-functions.js
+++ js/performance/nested-functions.js
@@ -1,18 +1,15 @@
-var calls = 999999999;
+var calls = 99999999;
 
 function notNested() {
-
     var start = new Date().getTime();
 
     function foo() { return 0; } 
 
-    function bar () {
-        for (var i = 0; i < calls; i++) {
-            foo();
-        }
-    }
+    function bar () { foo(); }
 
-    bar();
+    for (var i = 0; i < calls; i++) {
+        bar();
+    }
 
     console.log('Unnested took %s ticks', new Date().getTime() - start);
 }
@@ -21,19 +18,36 @@ function nested () {
     var start = new Date().getTime();
 
     function bar () {
-        for (var i = 0; i < calls; i++) {
-
-            function foo() { return 0; } 
-
-            foo();
-        }
+        function foo() { return 0; } 
+        foo();
     }
 
-    bar();
+    for (var i = 0; i < calls; i++) {
+        bar();
+    }
 
     console.log('Nested took %s ticks', new Date().getTime() - start);
+}
+
+function nestedReturning () {
+    var start = new Date().getTime();
+
+    var bar = (function () {
+        function foo() { return 0; } 
+
+        return function () { foo(); };
+
+    })();
+
+    for (var i = 0; i < calls; i++) {
+        bar();
+    }
+
+    console.log('Nested returning took %s ticks', new Date().getTime() - start);
 }
 
 notNested();
 
 nested();
+
+nestedReturning();
