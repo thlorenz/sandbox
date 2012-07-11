20d32133b9ec1ef78bd07a27d056a0667c4f549a
Thorsten Lorenz <thlorenz@gmx.de>
2012-07-11 08:23:50 -0400

    chain of three

--- js/streams/cruise.js
+++ js/streams/cruise.js
@@ -4,25 +4,31 @@ var util = require('util')
   , cruise
   ;
 
-var Cruise = function () {
+var Cruise = function (func) {
+//    , args = Array.prototype.slice.call(arguments)
 
   Stream.call(this);
   this.writeable = this.readable = true;
 
-  var self = this
-    , args = Array.prototype.slice.call(arguments)
-    , func = args.shift();
+  var self = this;
+
+  var callback = function (err, res) {
+    console.log(arguments);
+         if (err) self.emit('error', err);
+    else if (res) self.emit('data', res);
+    else          self.emit('next');     
+  };
 
   self.on('pipe', function (pipe) {
     pipe
-      .on('data', function (data) {
-        if (data) args = args.concat(data);
-
-        func.apply(self, args.concat(function (err, res) {
-          if (err)  self.emit('error', err); 
-          else      self.emit('data', res);
-        }));
+      .on('data', function (res) {
+        console.log('data', res);
+        func.apply(self, [ res, callback]);
       })  
+      .on('next', function () {
+        console.log('next');
+        func.call(self, [ callback ]);
+      })
       .on('error', function (err) {
         console.log('error', err);
         self.emit('error', err);  
@@ -58,7 +64,12 @@ function chain(streams) {
 }
 
 
-var read = new Cruise(fs.readFile, './cruise.js', 'ascii');
-var log  = new Cruise(function log(x, cb) { console.log(x); cb(); });
+var list = new Cruise(fs.readdir);
+var read = new Cruise(function read(files, cb) { 
+  files.forEach(function (file) {
+    fs.readFile(file, 'ascii', cb);
+  });
+});
+var log  = new Cruise(function log(x, cb) { console.log(arguments); cb(); });
 
-chain([ read, log ]).push(null);
+chain([ list, read, log ]).push('.');
