426caa863045cfff7b923c1e6f3921134d57760b
Thorsten Lorenz <thlorenz@gmx.de>
2012-07-11 07:34:18 -0400

    chaining two functions

--- js/streams/cruise.js
+++ js/streams/cruise.js
@@ -1,24 +1,64 @@
 var util = require('util')
   , fs = require('fs')
   , Stream = require('stream').Stream
+  , cruise
   ;
 
 var Cruise = function () {
+
   Stream.call(this);
+  this.writeable = this.readable = true;
+
   var self = this
     , args = Array.prototype.slice.call(arguments)
     , func = args.shift();
 
-  console.log('func:', func);
-  console.log('arguments', arguments);
-  
-  func.apply(this, args.concat(function () {
-    console.log('read', arguments);
-  }));
+  self.on('pipe', function (pipe) {
+    pipe
+      .on('data', function (data) {
+        if (data) args = args.concat(data);
 
-  // function is assumed to at least take one callback
+        func.apply(self, args.concat(function (err, res) {
+          if (err)  self.emit('error', err); 
+          else      self.emit('data', res);
+        }));
+      })  
+      .on('error', function (err) {
+        console.log('error', err);
+        self.emit('error', err);  
+      });
+  });
+};
+
+util.inherits(Cruise, Stream);
+
+function Pusher () {
+  Stream.call(this);
+  this.writeable = this.readable = true;
+}
+util.inherits(Pusher, Stream);
+
+function chain(streams) {
+  if (streams.length < 2) throw new Error ('need at least two streams to chain them');
+
+  var pusher = new Pusher();
+  streams.unshift(pusher);
+
+  for (var i = 0; i < streams.length - 1; i++) {
+    var pub = streams[i]
+      , sub = streams[i + 1]; 
+
+    pub.pipe(sub);
+  }
+  return {
+    push: function (data) {
+      pusher.emit('data', data);
+    }
+  };
 }
 
 
+var read = new Cruise(fs.readFile, './cruise.js', 'ascii');
+var log  = new Cruise(function log(x, cb) { console.log(x); cb(); });
 
-new Cruise(fs.readFile, './cruise.js', 'utf-8');
+chain([ read, log ]).push(null);
